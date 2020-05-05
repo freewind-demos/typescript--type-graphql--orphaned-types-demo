7287901215251fee9a9ca97928ba4cc02c4e425c
Freewind <nowindlee@gmail.com>
2020-05-05 22:35:45 +0800

    improve

--- src/hello.ts
+++ src/hello.ts
@@ -1,17 +1,15 @@
-import path from 'path';
 import 'reflect-metadata';
-import {buildSchema} from 'type-graphql';
+import path from 'path';
+import {buildSchemaSync} from 'type-graphql';
 
 import DummyResolver from './dummy/DummyResolver';
 import MyOrphanedType from './MyOrphanedType';
 
-async function main() {
-  const schema = await buildSchema({
-    resolvers: [DummyResolver],
-    validate: false, // https://github.com/MichalLytek/type-graphql/issues/150
-    orphanedTypes: [MyOrphanedType],
-    emitSchemaFile: path.resolve(__dirname, '../dist/schema.graphql')
-  });
-}
+const schema = buildSchemaSync({
+  resolvers: [DummyResolver],
+  validate: false, // https://github.com/MichalLytek/type-graphql/issues/150
+  orphanedTypes: [MyOrphanedType],
+  emitSchemaFile: path.resolve(__dirname, '../dist/schema.graphql')
+})
 
-main();
+console.log(schema.getTypeMap());
